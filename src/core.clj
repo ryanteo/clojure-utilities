37df02687dae03eecfa66afe04975ec1c7d98c5f
Ryan Teo <ryan.teo@gmail.com>
2011-10-12 15:48:14 +0800

    Second commit

--- src/core.clj
+++ src/core.clj
@@ -3,7 +3,7 @@
 ;A list of string definitions for the special characters
 (def str-curlybraces-1 (str (get special 2)))          
 (def str-curlybraces-2 (str (get special 4)))
-(def str-hex1 (str (get special 1)))
+(def str-hex (str (get special 1)))
 (def str-single-quote (str (get special 3)))
 (def str-dollar-sign (str (get special 5)))
 (def str-backslash (str (get special 6)))
